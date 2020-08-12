aafec1558c014066a522f716e465ccf0f8952ade
John Tran <johntran@gmail.com>
2020-08-11 22:09:47 -0600

    swap filenames

--- powermeterDisplay/powermeterDisplay.ino
+++ powermeterDisplay/powermeterDisplay.ino
@@ -1,186 +1,216 @@
-// HX711 Arduino Library - Version: Latest
-// For strain gauges
-#include <HX711.h>
-
-// ArduinoBLE - Version: Latest
-// For bluetooth
 #include <ArduinoBLE.h>
 
-BLEService powerMeterService("1818");
-BLEUnsignedCharCharacteristic powerChar("2A63", BLERead | BLENotify );
-BLEUnsignedCharCharacteristic resistanceChar("2AD6", BLERead | BLENotify );
-BLEUnsignedCharCharacteristic cadenceChar("2A5B", BLERead | BLENotify );
+// https://github.com/sigvaldm/SevenSeg
+#include <SevenSeg.h>
+//                           a, b, c, d, e, f, g
+SevenSeg      powerDisplay (A0, 2,A7,A3,A2,A1,A6);
+SevenSeg resistanceDisplay (A0, 2,A7,A3,A2,A1,A6);
+SevenSeg    cadenceDisplay (A0, 2,A7,A3,A2,A1,A6);
+// the display i bought:    11, 7, 4, 2, 1,10, 5 (3 is DP), selects are 12, 9, 8
 
-// Arduino_LSM9DS1 - Version: Latest
-// For IMU
-#include <Arduino_LSM9DS1.h>
+const int numOfDigits=3;
+int      digitPinsPower[numOfDigits]={ 3, 4, 5};
+int digitPinsResistance[numOfDigits]={11, 6, 7};
+int    digitPinsCadence[numOfDigits]={ 8, 9,10};
+
+float displayFrequency = 100;
+//float displayFrequency = 50;
+float digitDelay = 1000000.0f / (3.0f * numOfDigits * displayFrequency);
+//8 displays, so T = 1/nF = 1/(8*100) = 1250 us
+//float digitDelay = 1000;
 
 int counter = 0;
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = A7;
-const int LOADCELL_SCK_PIN = A6;
+const int DEBUG = 0;
 
-HX711 scale;
-float offset, calibrationFactor;
-float accelerationSampleRate, gyroscopeSampleRate;
+void displayPowerResistanceCadence (unsigned char power, unsigned char resistance, unsigned char cadence) {
+  /*
+   * Product Name: LED Digital Display Tube; 
+   * Type: Common Cathode; Model: 3631AH
+   * Common Cathode: 12-9-8; Digital Display: 3 Digit; Digital Number: 3 Bit 7 Segment; Emitted Color: Red
+   * Pin Number: 11; Continuous Forward Current: 20mA; Average Forward Voltage: 2V; Power Consumption: 36mW
+   */
 
-//unsigned long previousMillis = 0;
+  powerDisplay.write(power);
+  powerDisplay.clearDisp();
+
+  resistanceDisplay.write(resistance);
+  resistanceDisplay.clearDisp();
+
+  cadenceDisplay.write(cadence);
+  cadenceDisplay.clearDisp();
+
+}
+
 
 void setup() {
-  Serial.begin(9600);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  if (DEBUG) Serial.begin(9600);
 
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
+  powerDisplay.setDutyCycle(displayFrequency);
+  powerDisplay.setDigitPins(numOfDigits, digitPinsPower);
+  powerDisplay.setRefreshRate(displayFrequency);
+  //powerDisplay.setDigitDelay(digitDelay);
+  powerDisplay.setCommonCathode();
+  
+  resistanceDisplay.setDutyCycle(displayFrequency);
+  resistanceDisplay.setDigitPins(numOfDigits, digitPinsResistance);
+  resistanceDisplay.setRefreshRate(displayFrequency);
+  //resistanceDisplay.setDigitDelay(digitDelay);
+  resistanceDisplay.setCommonCathode();
 
-  offset = 0;
-  calibrationFactor = 0;
+  cadenceDisplay.setDutyCycle(displayFrequency);
+  cadenceDisplay.setDigitPins(numOfDigits, digitPinsCadence);
+  cadenceDisplay.setRefreshRate(displayFrequency);
+  //cadenceDisplay.setDigitDelay(digitDelay);
+  cadenceDisplay.setCommonCathode();
 
-  accelerationSampleRate = IMU.accelerationSampleRate();
-  gyroscopeSampleRate    = IMU.gyroscopeSampleRate();
-
-
-  //bluetooth setup
+  // initialize the BLE hardware
   if ( !BLE.begin() ) {
-    Serial.println("starting BLE failed!");
+    if (DEBUG) Serial.println("starting BLE failed!");
     while (1);
   } else {
-    Serial.println ("Bluetooth startup succeeded");
+    if (DEBUG) Serial.println ("Bluetooth startup succeeded");
   }
 
-  BLE.setLocalName("powerMeterService");
-  BLE.setAdvertisedService(powerMeterService);
-  powerMeterService.addCharacteristic(powerChar);
-  powerMeterService.addCharacteristic(resistanceChar);
-  powerMeterService.addCharacteristic(cadenceChar);
-  BLE.addService(powerMeterService);
+  // start scanning for peripherals
+  BLE.scanForUuid("1818");
 
-  BLE.advertise();
-  Serial.println("Bluetooth device active, waiting for connections...");
-}
-
-float computeResistance ( const float &power, const float &rpm ) {
-
-  // from https://www.reddit.com/r/pelotoncycle/comments/b0bulz/how_the_peloton_bike_calculates_output_and_speed/
-  // Output ~= (Cadence - 35) * (Resistance/100)2.5 * 24
-  // Speed ~= (Cadence - 35)0.4 * (Resistance/100) * 9 + 0.4
-  // Output ~= Speed2.5 / 10
-
-  // Output ~= (Cadence - 35) * (Resistance/100)2.5 * 24
-  // (Resistance/100)2.5 = Output / (24 * Cadence-35)
-  // Resistance = 100 * Output / (2.5*24*(cadence-35))
-  float r = 5 * power / (3 * (rpm - 35));
-  return r;
-}
-
-unsigned char float2uchar (const float &val, const char * name) {
-  unsigned char val_uchar = (unsigned char)val;
-  if (1) {
-    Serial.print(name);
-    Serial.print("=");
-    Serial.print(val_uchar);
-    Serial.print(",");
-    Serial.print(val);
-    Serial.print("\t");
-  }
-  return val_uchar;
-}
-
-void writeToBluetooth (unsigned char power, unsigned char resistance, unsigned char cadence) {
-    powerChar.writeValue(power);
-    resistanceChar.writeValue(resistance);
-    cadenceChar.writeValue(cadence);
+  //BLE.debug(Serial); 
 }
 
 void loop() {
 
-  delay(1000);
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
 
-  //bluetooth stuff
-  BLEDevice central = BLE.central();
-
-  if (central) {
-  
-    Serial.print("Connected to central: ");
-    Serial.print(central.address());
-
-    unsigned char power_uchar=0, power_uchar_new=0;
-    unsigned char resistance_uchar=0, resistance_uchar_new=0;
-    unsigned char cadence_uchar=0, cadence_uchar_new=0;
+  if (peripheral) {
+    if (DEBUG) Serial.print("Bluetooth connected to ");
+    if (DEBUG) Serial.print(peripheral.localName());
     
-    while (central.connected()) {
-      
-      Serial.print("Counter = ");
-      Serial.print(counter++);
-    
-      // get gyro_z
-      float gyro_x, gyro_y, gyro_z = 0;
-      if (IMU.gyroscopeAvailable()) {
-        IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
-    
-        Serial.print("\tGyro=\t");
-        Serial.print(gyro_x);
-        Serial.print('\t');
-        Serial.print(gyro_y);
-        Serial.print('\t');
-        Serial.print(gyro_z);
-      } else {
-        Serial.print("gyro not available\t");
-      }
-      Serial.print("\t");
-      
-      /*
-        float accel_x, accel_y, accel_z = 0;
-        if (IMU.accelerationAvailable()) {
-        IMU.readAcceleration(accel_x, accel_y, accel_z);
-    
-        Serial.print("\tAccel=\t");
-        Serial.print(accel_x);
-        Serial.print('\t');
-        Serial.print(accel_y);
-        Serial.print('\t');
-        Serial.print(accel_z);
-        } else Serial.print("accel not available\t");
-      */
-      float GyZ, GyZ_Radians, cadence;
-      GyZ = gyro_z;
-      /* Time to do some maths */
-      //GyZ_Radians = -GyZ * (1 / 32.8) * (3.14 / 180); // Convert into radians / sec
-      cadence = -GyZ * 0.0051; // Convert into revs / sec
-    
-      /* Get values from the strain gauges */
-      //float reading = scale.read();
-      //Serial.print("Scale Reading:\t"); Serial.println(reading);
-      float power = 100.0f; //XXX
-    
-      cadence *= 10; // XXX temporarily inflate
-      float resistance = computeResistance(power, cadence);
-    
-      power_uchar_new      = float2uchar(power, "Power");
-      cadence_uchar_new    = float2uchar(cadence, "Cadence");
-      resistance_uchar_new = float2uchar(resistance, "Resistance");
-
-      //Serial.print("writing to bluetooth\n");
-      //writeToBluetooth(power_uchar, resistance_uchar, cadence_uchar);
-      if (power_uchar_new != power_uchar) {
-        power_uchar = power_uchar_new;
-        powerChar.writeValue(power_uchar);
-      }
-      if (resistance_uchar_new != resistance_uchar) {
-        resistance_uchar = resistance_uchar_new;
-        resistanceChar.writeValue(resistance_uchar);
-      }
-      if (cadence_uchar_new != cadence_uchar) {
-        cadence_uchar = cadence_uchar_new;
-        cadenceChar.writeValue(cadence_uchar);
-      }
-
+    if (peripheral.localName() != "powerMeterService") {
+      return;
     }
-  }
-  //Serial.println("");
-  return;
 
+    // stop scanning
+    BLE.stopScan();
+
+    if (peripheral.connect()) {
+      if (DEBUG) Serial.println (" Bluetooth connected.");
+
+      // discover peripheral attributes
+      if (DEBUG) Serial.println("Discovering attributes ...");
+      if (peripheral.discoverAttributes()) {
+        if (DEBUG) Serial.println("Attributes discovered");
+      } else {
+        if (DEBUG) Serial.println("Attribute discovery failed!");
+        peripheral.disconnect();
+        return;
+      }
+      
+      //int characteristicCount = peripheral.characteristicCount();
+      //Serial.print(characteristicCount);
+      //Serial.println(" characteristics discovered");
+      
+      BLECharacteristic powerChar = peripheral.characteristic("2A63");
+      BLECharacteristic resistanceChar = peripheral.characteristic("2AD6");
+      BLECharacteristic cadenceChar = peripheral.characteristic("2A5B");
+
+      // subscribe to the powerChar characteristic
+      if (DEBUG) Serial.println("Subscribing to powerChar ...");
+      if (!powerChar) {
+        if (DEBUG) Serial.println("no powerChar found!");
+        peripheral.disconnect();
+        return;
+      } else if (!powerChar.canSubscribe()) {
+        if (DEBUG) Serial.println("powerChar is not subscribable!");
+        peripheral.disconnect();
+        return;
+      } else if (!powerChar.subscribe()) {
+        if (DEBUG) Serial.println("powerChar subscription failed!");
+        peripheral.disconnect();
+        return;
+      }
+      
+      // subscribe to the resistanceChar characteristic
+      if (DEBUG) Serial.println("Subscribing to resistanceChar ...");
+      if (!resistanceChar) {
+        if (DEBUG) Serial.println("no resistanceChar found!");
+        peripheral.disconnect();
+        return;
+      } else if (!resistanceChar.canSubscribe()) {
+        if (DEBUG) Serial.println("resistanceChar is not subscribable!");
+        peripheral.disconnect();
+        return;
+      } else if (!resistanceChar.subscribe()) {
+        if (DEBUG) Serial.println("resistanceChar subscription failed!");
+        peripheral.disconnect();
+        return;
+      }
+
+      // subscribe to the cadenceChar characteristic
+      if (DEBUG) Serial.println("Subscribing to cadenceChar ...");
+      if (!cadenceChar) {
+        if (DEBUG) Serial.println("no cadenceChar found!");
+        peripheral.disconnect();
+        return;
+      } else if (!cadenceChar.canSubscribe()) {
+        if (DEBUG) Serial.println("cadenceChar is not subscribable!");
+        peripheral.disconnect();
+        return;
+      } else if (!cadenceChar.subscribe()) {
+        if (DEBUG) Serial.println("cadenceChar subscription failed!");
+        peripheral.disconnect();
+        return;
+      }
+      
+      unsigned char power=68, resistance=69, cadence=70;
+      unsigned char newPower=68, newResistance=69, newCadence=70;
+      int powerBytesRead=7, resistanceBytesRead=8, cadenceBytesRead=9;
+
+      powerChar.readValue(power);
+      resistanceChar.readValue(resistance);
+      cadenceChar.readValue(cadence);
+      
+      while (peripheral.connected()) {
+        if (powerChar.valueUpdated()) {
+          powerBytesRead = powerChar.readValue(newPower);
+          power = newPower;
+        }
+        if (resistanceChar.valueUpdated()) {
+          resistanceBytesRead = resistanceChar.readValue(newResistance);
+          resistance = newResistance;
+        }
+        if (cadenceChar.valueUpdated()) {
+          cadenceBytesRead = cadenceChar.readValue(newCadence);
+          cadence = newCadence;
+        }
+      
+        if (DEBUG) Serial.print(" Bytes read: "); Serial.print(powerBytesRead); Serial.print(resistanceBytesRead); Serial.println(cadenceBytesRead);
+        
+        displayPowerResistanceCadence (power, resistance, cadence);
+        if (DEBUG) {
+          Serial.print(" P="); Serial.print(power);
+          Serial.print(", R="); Serial.print(resistance);
+          Serial.print(", C="); Serial.println(cadence);
+        }
+      }
+    }
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("1818");
+  }
+  else {  //if (peripheral) 
+    powerDisplay.write("Hi ");
+    powerDisplay.clearDisp();
+
+    resistanceDisplay.write("CAL");
+    resistanceDisplay.clearDisp();
+
+    cadenceDisplay.write("STR");
+    cadenceDisplay.clearDisp();
+
+    if (DEBUG) Serial.println("bluetooth not connected.");
+  }
+  
+  //delay(500);
 }
